c71b8102c83f690ef615b15f73b09785e2daefa6
afrianto.nasution@gmail.com <afrianto.nasution@gmail.com>
2022-01-12 22:00:57 +0700

    membuat scroll tabs jumping

--- src/components/TabNav.vue
+++ src/components/TabNav.vue
@@ -16,6 +16,7 @@
                                 :label="rows" 
                                 no-caps
                                 class="q-pr-lg"
+                                @click="goto(rows)"
                             /> 
                         
                         </q-tabs>
@@ -33,11 +34,11 @@
                                 v-intersection="onIntersection"
                                 >
                                 <q-item-section class="text-center"> 
-                                    <div style="margin-bottom:20px; margin-left:-45px;">  
+                                    <div style="margin-bottom:30px; margin-left:-45px;">  
                                         <div class="text-h4 text-bold text-left q-pb-xl ">
                                             {{ labels[n] }} 
                                         </div>
-                                        <card/> 
+                                        <div :ref="`${labels[n]}`"><card/></div> 
                                     </div> 
                                 </q-item-section>
                                 </q-item>
@@ -70,8 +71,7 @@ export default {
                 'Addon'
             ],
             thresholds : ['','1','2','3','4','5','6','7','8','9','10'],
-            isAddClass: false,
-            nilai : 1
+            isAddClass: false 
         }
     },
     created() {
@@ -82,13 +82,11 @@ export default {
     },
     methods:{
         onIntersection (entry) { 
-            if (entry.isIntersecting === true) {
-                console.log(entry.target.dataset.id)
+            if (entry.isIntersecting === true) { 
                 this.tab = `${this.thresholds[entry.target.dataset.id]}`  
             } 
         },
-        add (i) {    
-           
+        add (i) {  
             if(i > 1){
                 this.isAddClass = true;
             }else{
@@ -101,6 +99,9 @@ export default {
             }else{
                 this.isAddClass = false; 
             }
+        },
+        goto(refName) {
+            this.$refs[refName].scrollIntoView({behavior: 'smooth', block: "end", inline: "nearest"});  
         }
     } 
 }
